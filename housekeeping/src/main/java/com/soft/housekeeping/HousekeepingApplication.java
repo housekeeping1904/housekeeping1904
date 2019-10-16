7dbe15e942187069d8332cb2a9d5ae5a637c5afe
yukaGit <1264201091@qq.com>
2019-10-16 23:09:27 +0800

    Git测试，第一次提交

---
+++ housekeeping/src/main/java/com/soft/housekeeping/HousekeepingApplication.java
@@ -0,0 +1,15 @@
+package com.soft.housekeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousekeepingApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(HousekeepingApplication.class, args);
+	}
+
+}
