da32510c2896311e0afa9f5b94f837359b1bb117
yukaGit <1264201091@qq.com>
2019-10-17 15:17:46 +0800

    zq 第一次提交

--- housekeeping/src/main/java/com/soft/housekeeping/HousekeepingApplication.java
+++ housekeeping/src/main/java/com/soft/housekeeping/HousekeepingApplication.java
@@ -1,9 +1,11 @@
 package com.soft.housekeeping;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.soft.housekeeping.mapper")
 public class HousekeepingApplication
 {
 
